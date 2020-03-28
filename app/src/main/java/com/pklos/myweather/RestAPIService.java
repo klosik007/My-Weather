2d68bacfe165c55540b03546c17cb42348db3005
Przemyslaw Klos <pklos1992@gmail.com>
2020-03-28 22:23:26 +0100

    changed getStream into streamable method, deleted unnecesessary file readers methods

--- app/src/main/java/com/pklos/myweather/RestAPIService.java
+++ app/src/main/java/com/pklos/myweather/RestAPIService.java
@@ -1,71 +1,25 @@
 package com.pklos.myweather;
 
-import android.util.JsonReader;
-import android.util.Log;
-
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.io.Reader;
-import java.net.HttpURLConnection;
 import java.net.URL;
 
 class RestAPIService {
-    static Reader getStream(String url) throws IOException {
-        URL obj = new URL(url);
-        HttpURLConnection connection = (HttpURLConnection)obj.openConnection();
-        InputStream resBody;
-        connection.setRequestMethod("GET");
-        int responseCode = connection.getResponseCode();
-
-        if (responseCode == HttpURLConnection.HTTP_OK) {
-           resBody = connection.getInputStream();
-        } else
-            return null;
-
-        connection.disconnect();
-
-        Reader resBodyReader = new InputStreamReader(resBody, "UTF-8");
-
-        return resBodyReader;
-    }
-
-    static InputStreamReader getStream2(String url) throws IOException {
-        URL obj = new URL(url);
-        HttpURLConnection connection = (HttpURLConnection)obj.openConnection();
-        InputStream resBody;
-        connection.setRequestMethod("GET");
-        int responseCode = connection.getResponseCode();
-
-        if (responseCode == HttpURLConnection.HTTP_OK) {
-            resBody = connection.getInputStream();
-        } else
-            return null;
-
-        connection.disconnect();
-
-//        JsonReader resBodyReader = new JsonReader(new InputStreamReader(resBody, "UTF-8"));
-        InputStreamReader resBodyReader = new InputStreamReader(resBody, "UTF-8");
-        return resBodyReader;
-    }
-
-    public static void readJsonStream(JsonReader in) throws IOException {
-        int message = -1;
+    public static String getStream(String urlString) throws Exception {
+        BufferedReader reader = null;
         try {
-            in.beginObject();
-            while (in.hasNext()){
-                String name = in.nextName();
-                if (name.equals("message")){
-                    message = in.nextInt();
-                    break;
-                }else{
-                    in.skipValue();
-                }
-            }
-            Log.d("message", String.valueOf(message));
+            URL url = new URL(urlString);
+            reader = new BufferedReader(new InputStreamReader(url.openStream()));
+            StringBuffer buffer = new StringBuffer();
+            int read;
+            char[] chars = new char[1024];
+            while ((read = reader.read(chars)) != -1)
+                buffer.append(chars, 0, read);
+
+            return buffer.toString();
         } finally {
-            in.endObject();
-            in.close();
+            if (reader != null)
+                reader.close();
         }
     }
 }
