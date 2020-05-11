7b1574cb11c938952e04247e6d2390c78ea154d4
Przemyslaw Klos <pklos1992@gmail.com>
2020-05-11 16:56:00 +0200

    AddLocationActivity java file created

---
+++ app/src/main/java/com/pklos/myweather/AddLocationActivity.java
@@ -0,0 +1,20 @@
+package com.pklos.myweather;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class AddLocationActivity extends AppCompatActivity {
+
+
+    public void searchCityButton(View view){
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_location);
+    }
+}
