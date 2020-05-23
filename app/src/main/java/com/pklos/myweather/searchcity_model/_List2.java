16200b7fc6fff7be2fed06cec4dd2b235461ebcc
Przemyslaw Klos <pklos1992@gmail.com>
2020-05-23 20:53:58 +0200

    model for list array in city search JSON

---
+++ app/src/main/java/com/pklos/myweather/searchcity_model/_List2.java
@@ -0,0 +1,11 @@
+package com.pklos.myweather.searchcity_model;
+
+import com.pklos.myweather.weatherforecast_model.Coord;
+
+public class _List2 {
+    public int id;
+
+    public String name;
+
+    public Coord coord;
+}
