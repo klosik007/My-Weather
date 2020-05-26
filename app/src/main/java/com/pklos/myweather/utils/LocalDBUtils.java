27209b310552057468fa3f0e71b76b8520c8f223
Przemyslaw Klos <pklos1992@gmail.com>
2020-05-26 23:07:21 +0200

    DB utils

---
+++ app/src/main/java/com/pklos/myweather/utils/LocalDBUtils.java
@@ -0,0 +1,40 @@
+package com.pklos.myweather.utils;
+
+import android.content.Context;
+import android.util.Log;
+
+import com.pklos.myweather.locations_database.LocationsDB;
+import com.pklos.myweather.locations_database.MyWeatherExecutors;
+import com.pklos.myweather.locations_model.Location;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Callable;
+
+public class LocalDBUtils {
+    public static void insertDataToDB(Context context, int id, String cityName){
+        final LocationsDB appDB = LocationsDB.getInstance(context);
+        final Location location = new Location(cityName, String.valueOf(id), false);
+        MyWeatherExecutors.getInstance().getDiskIO().execute(new Runnable(){
+            @Override
+            public void run() {
+                appDB.locationsDao().insertLocation(location);
+            }
+        });
+    }
+
+    public static void fetchLocationsData(Context context){
+        final LocationsDB appDB = LocationsDB.getInstance(context);
+
+        MyWeatherExecutors.getInstance().getDiskIO().execute(new Runnable(){
+            @Override
+            public void run() {
+                final List<Location> locations = appDB.locationsDao().getLocationsList();
+                for (Location location : locations){
+                    Log.d("Locations", location.getId() + location.getCityName() + location.getCityID() + location.isDefault());
+                }
+            }
+        });
+    }
+}
