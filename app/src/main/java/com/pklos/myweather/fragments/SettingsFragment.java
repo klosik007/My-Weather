d280c77ea6fd18f4ca62768611a50145c1330f0a
Przemyslaw Klos <pklos1992@gmail.com>
2020-06-23 18:15:15 +0200

    SettingsFragment separate class: loading available cities, on city touch handlers: delete record from DB

---
+++ app/src/main/java/com/pklos/myweather/fragments/SettingsFragment.java
@@ -0,0 +1,47 @@
+package com.pklos.myweather.fragments;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.preference.Preference;
+import androidx.preference.PreferenceCategory;
+import androidx.preference.PreferenceFragmentCompat;
+
+import com.pklos.myweather.R;
+import com.pklos.myweather.locations_database.LocationsDB;
+import com.pklos.myweather.locations_database.MyWeatherExecutors;
+import com.pklos.myweather.locations_model.Location;
+import com.pklos.myweather.utils.LocalDBUtils;
+
+import java.util.List;
+
+public class SettingsFragment extends PreferenceFragmentCompat {
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+        setPreferencesFromResource(R.xml.settings, rootKey);
+
+        final PreferenceCategory citiesCategory = (PreferenceCategory) findPreference("cities_to_delete");
+
+        final LocationsDB appDB = LocationsDB.getInstance(getContext());
+        MyWeatherExecutors.getInstance().getDiskIO().execute(new Runnable(){
+            @Override
+            public void run() {
+                final List<Location> locations = appDB.locationsDao().getLocationsList();
+                for (final Location location : locations){
+                    Preference cityToDelete = new Preference(getContext());
+                    cityToDelete.setTitle(location.getCityName());
+                    cityToDelete.setSummary(R.string.touch_city_tip);
+                    cityToDelete.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+                        @Override
+                        public boolean onPreferenceClick(Preference preference) {
+                            LocalDBUtils.deleteDataFromDB(getContext(), location);
+                            Toast.makeText(getContext(), R.string.on_city_remove, Toast.LENGTH_SHORT).show();
+                            return true;
+                        }
+                    });
+                    citiesCategory.addPreference(cityToDelete);
+                }
+            }
+        });
+    }
+}
