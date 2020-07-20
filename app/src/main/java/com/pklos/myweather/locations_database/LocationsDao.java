80e370d8482232c4b9d7949e52eb64b369b6d075
Przemyslaw Klos <pklos1992@gmail.com>
2020-07-20 22:52:25 +0200

    locations new queries (getDefaultLocation, updateiSDefaultLocation, updateDefaultCity)

--- app/src/main/java/com/pklos/myweather/locations_database/LocationsDao.java
+++ app/src/main/java/com/pklos/myweather/locations_database/LocationsDao.java
@@ -16,8 +16,14 @@ public interface LocationsDao {
     @Query("SELECT * FROM locations")
     List<Location> getLocationsList();
 
-//    @Query("SELECT cityName, isDefault FROM locations WHERE id LIKE :ID")
-//    List<Location> getLocation(int ID);
+    @Query("SELECT cityID FROM locations WHERE isDefault=:isDefault")
+    String getDefaultLocation(boolean isDefault);
+
+    @Query("UPDATE locations SET isDefault=:setDefault")
+    void updateIsDefaultToFalse(boolean setDefault);
+
+    @Query("UPDATE locations SET isDefault=:setDefault WHERE id=:id")
+    void updateDefaultCity(boolean setDefault, int id);
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     void insertLocation(Location... locations);
